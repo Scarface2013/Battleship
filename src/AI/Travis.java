a9bdff4f69348772d1382131e2c03829b3af0ecc
Travis Fletcher <mindscape11@gmail.com>
2016-06-07 15:06:56 -0400

    Added second AI test file

---
+++ src/AI/Travis.java
@@ -0,0 +1,12 @@
+package AI;
+
+import tech.tfletch.battleship.*;
+
+public class Travis extends AI{
+  public String toString(){
+    return "Travis";
+  }
+  public String Test(){
+    return "I am the best class ever";
+  }
+}
