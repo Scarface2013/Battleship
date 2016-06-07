c6f1e54869c7fb2a73aed08a66db1d6fb3ec0531
Travis Fletcher <mindscape11@gmail.com>
2016-06-07 15:11:08 -0400

    Added free will

--- src/MenuTester.java
+++ src/MenuTester.java
@@ -1,12 +1,20 @@
 package tech.tfletch.battleship.tests;
 
 import tech.tfletch.battleship.*;
+import java.util.Scanner;
 
 public class MenuTester{
   public static void main(String[] args){
     try{
+      Scanner in = new Scanner(System.in);
       Menu<AI> menu = new Menu<AI>("AI/");
-      AI o = menu.makeSelection(0);
+
+      for(String s : menu.drawMenu()){
+        System.out.println(s);
+      }
+      System.out.print("Please make a selction: ");
+      AI o = menu.makeSelection(in.nextInt());
+
       System.out.println(o.toString());
       System.out.println(o.Test());
     }
