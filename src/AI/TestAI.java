5a6e29845a2eec615982fe4fa75dc13bc3198785
Travis Fletcher <mindscape11@gmail.com>
2016-06-07 13:34:26 -0400

    Added test class

---
+++ src/AI/TestAI.java
@@ -0,0 +1,12 @@
+package AI;
+
+import tech.tfletch.battleship.*;
+
+public class TestAI extends AI{
+  public String toString(){
+    return "TestAI";
+  }
+  public String Test(){
+    return "It worked!";
+  }
+}
