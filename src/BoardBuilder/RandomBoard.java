e751995a44e3410379e17cc8958762fe392014c1
Travis Fletcher <mindscape11@gmail.com>
2016-07-03 23:21:02 -0400

    Fixed ignoring of board validity

--- src/BoardBuilder/RandomBoard.java
+++ src/BoardBuilder/RandomBoard.java
@@ -14,32 +14,39 @@ public class RandomBoard extends BoardBuilder{
     
     for(int i = 0; i<5;i++){
       boolean valid = false;
+      int num = 0;
+      int x = 0;
+      int y = 0;
+      int dir = 0;
       while(!valid){
-        int num = rand.nextInt(9999);
-        // Massage numbers into usable format
-        int x = num%10;
+        num = rand.nextInt(9999);
+        
+	// Massage numbers into usable format
+        x = num%10;
         num/=10;
-        int y = num%10;
+        y = num%10;
         num/=10;
-        int dir = num%4; // 0/1 = up/down and 2/3 = left/right
+        dir = num%4; // 0/1 = up/down and 2/3 = left/right
          
         valid = validate(x,y,dir,sizeMap[i]);
-        for(int j = 0; j < sizeMap[i]; j++){
-          if(dir == 0)
-            boardArray[x][y+j] = 1;
-          else if(dir == 1)
-            boardArray[x][y-j] = 1;
-          else if(dir == 2)
-            boardArray[x-j][y] = 1;
-          else if(dir == 3)
-            boardArray[x+j][y] = 1;
-        }
+      }
+      for(int j = 0; j < sizeMap[i]; j++){
+        if(dir == 0)
+          boardArray[x][y+j] = 1;
+        else if(dir == 1)
+          boardArray[x][y-j] = 1;
+        else if(dir == 2)
+          boardArray[x-j][y] = 1;
+        else if(dir == 3)
+          boardArray[x+j][y] = 1;
       }
     }
     try{
       return new Board(boardArray);
     }catch(Exception e){
+      e.printStackTrace();
       return null;
+
     }
   }
 
