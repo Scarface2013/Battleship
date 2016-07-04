1d552f96ad973ec9e2554f5a194b1c5a43c2e999
Travis Fletcher <mindscape11@gmail.com>
2016-07-03 23:21:54 -0400

    Added no-args buildBoard

--- src/BoardBuilder.java
+++ src/BoardBuilder.java
@@ -2,4 +2,7 @@ package tech.tfletch.battleship;
 
 public abstract class BoardBuilder{
   public abstract Board buildBoard(int seed);
+  public Board buildBoard(){
+    return buildBoard((int)(Math.random()*2147483647));
+  }
 }
